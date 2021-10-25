d99a5e1d0cf5d66217df9e305c65934c45b8b2e8
Leoguzs <lguzs96@gmail.com>
2021-10-25 08:01:04 -0500

    ejercicios de REACT

---
+++ src/components/books.js
@@ -0,0 +1,22 @@
+export let books = [
+  {
+    id: 1,
+    img: "https://i1.wp.com/www.mexmads.com/wp-content/uploads/2020/04/YPD3NNJ4OZFU5CLBPWHI5RVU3A.jpg?fit=980%2C528",
+    character: "Naruto",
+    description:
+      "Naruto wants to prove to the world that you have to keep triying and doing your best to be who you want to be.",
+  },
+  {
+    id: 2,
+    img: "https://sm.ign.com/t/ign_latam/screenshot/default/sske_fzbq.1280.png",
+    character: "Sasuke",
+    description:
+      "Sasuke has family issues. Actually, he does not have a family, all because of his brother: Itachi Uchiha",
+  },
+  {
+    id: 3,
+    img: "https://i2.wp.com/codigoespagueti.com/wp-content/uploads/2021/05/cosplay-de-sakura-haruno-de-naruto-alexy-sky.jpg?fit=1280%2C720&quality=80&ssl=1",
+    character: "Sakua",
+    description: "She is cool",
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//CSS
+import "./components/index.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import { books } from "./components/books";
+//setup bars
+
+/* const description =
+  "Naruto has proven to the world that you have to keep triying and doing your best to be who you want to be";
+const character = "Naruto";
+const img =
+  "https://i1.wp.com/www.mexmads.com/wp-content/uploads/2020/04/YPD3NNJ4OZFU5CLBPWHI5RVU3A.jpg?fit=980%2C528"; */
+
+//Components
+const Booklist = () => {
+  //Este sería como el contedor
+  return (
+    <section className="booklist">
+      {books.map((book) => {
+        return <Book key={book.id} {...book}></Book>; //su propiedad es book
+      })}
+    </section>
+  );
+};
+
+const Book = ({ img, character, description }) => {
+  //las propiedades son las características de mi funcion/clase, en este caso son en forma de objeto, así que puedo desestructurar
+  //event: attribute, eventHandler
+  //onClick onMouseOver
+  const clickHandler = (e) => {
+    console.log(e);
+    console.log(e.target);
+  };
+
+  const complexExample = (character) => {
+    console.log(character);
+  };
+  return (
+    <article
+      className="book"
+      onMouseOver={() => {
+        console.log(character);
+      }}
+    >
+      <img src={img} width="400" alt="" />
+      <h1 onClick={() => alert(character)}>
+        <strong>{character}</strong>
+      </h1>
+      <p>{description}</p>
+      <button onClick={clickHandler} type="button">
+        Touch it
+      </button>{" "}
+      <br />
+      <button type="button" onClick={() => complexExample(character)}>
+        More complex example
+      </button>
+    </article>
+  );
+};
+
+ReactDom.render(<Booklist />, document.getElementById("root"));
